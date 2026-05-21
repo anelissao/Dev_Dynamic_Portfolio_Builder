c691c66453679fb690c6a1deddac414044cfe33e
anelissao <anouarelissaoui20@gmail.com>
2026-05-21 14:04:30 +0000

    auth.ts

---
+++ src/auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import Github from "next-auth/providers/github"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+import {prisma} from "@/lib/db"
+
+
+export const {handlers, signIn, signOut, auth} = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [Github]
+})
