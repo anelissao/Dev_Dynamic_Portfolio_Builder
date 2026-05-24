d1351d7f98e5497335523c49c64cd4366e5ab45f
anelissao <anouarelissaoui20@gmail.com>
2026-05-24 18:13:28 +0000

    starter working on image upload using Uploadthing, created the core.ts

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,21 @@
+import {createUploadthing, type FileRouter} from "uploadthing/next"
+import { auth } from "@/auth"
+import { metadata } from "@/app/layout"
+
+const f = createUploadthing()
+
+export const ourFileRouter = {
+    avatarUploader: f({
+        image: {maxFileSize: "4MB", maxFileCount: 1},
+    })
+    .middleware(async () => {
+        const session = await auth()
+        if (!session?.user?.id) throw new Error("Not Authenticated")
+        return {userId: session.user.id}
+    })
+    .onUploadComplete(async({metadata, file}) => {
+        console.log("Upload complete for", metadata.userId, file.url)
+    }),
+} satisfies FileRouter
+
+export type OurFileRouter = typeof ourFileRouter
