40117c29e292186d7f202523163d94a24e93f866
anelissao <anouarelissaoui20@gmail.com>
2026-05-28 03:32:38 +0000

    created the projectImage endpoint to upload projects images using uploadThing

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,4 +1,4 @@
-import {createUploadthing, type FileRouter} from "uploadthing/next"
+import { createUploadthing, type FileRouter } from "uploadthing/next"
 import { auth } from "@/auth"
 import { metadata } from "@/app/layout"
 
@@ -6,16 +6,27 @@ const f = createUploadthing()
 
 export const ourFileRouter = {
     avatarUploader: f({
-        image: {maxFileSize: "4MB", maxFileCount: 1},
+        image: { maxFileSize: "4MB", maxFileCount: 1 },
     })
-    .middleware(async () => {
-        const session = await auth()
-        if (!session?.user?.id) throw new Error("Not Authenticated")
-        return {userId: session.user.id}
-    })
-    .onUploadComplete(async({metadata, file}) => {
-        console.log("Upload complete for", metadata.userId, file.ufsUrl)
-    }),
+        .middleware(async () => {
+            const session = await auth()
+            if (!session?.user?.id) throw new Error("Not Authenticated")
+            return { userId: session.user.id }
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            console.log("Upload complete for", metadata.userId, file.ufsUrl)
+        }),
+    projectImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+        .middleware(async () => {
+            const session = await auth();
+            if (!session?.user?.id) throw new Error("Unauthorized");
+            return { userId: session.user.id };
+        })
+        .onUploadComplete(async ({ metadata, file }) => {
+            console.log("Upload complete for userId:", metadata.userId);
+            console.log("File URL:", file.ufsUrl);
+            return { uploadedBy: metadata.userId, url: file.ufsUrl };
+        }),
 } satisfies FileRouter
 
 export type OurFileRouter = typeof ourFileRouter
